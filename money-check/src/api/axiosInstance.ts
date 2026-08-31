cdae86a4fbcd80effde62405663c9d27001e9a59
lerray-code <83026492+lerray-code@users.noreply.github.com>
2026-08-31 15:16:58 +0300

    Update axiosInstance.ts add server api

--- money-check/src/api/axiosInstance.ts
+++ money-check/src/api/axiosInstance.ts
@@ -2,7 +2,11 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+});
+
+export const api = axios.create({
+  baseURL: API_URL,
 });
 
 api.interceptors.response.use(
