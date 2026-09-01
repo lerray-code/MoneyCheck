f1e9f3694501c57a6e66be4a0b37cfa93d24ec69
@lera-yaskevich <leray382@gmail.com>
2026-09-01 19:39:56 +0300

    dell eff

--- money-check/src/hooks/useDashboardData.ts
+++ money-check/src/hooks/useDashboardData.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getIncomes } from "../api/incomeApi";
 import { getExpenses } from "../api/expenseApi";
 import { getBudgets } from "../api/budgetApi";
